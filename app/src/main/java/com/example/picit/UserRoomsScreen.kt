d1ae396521d734337e9f59839dce66fd2686437f
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-25 15:02:59 +0000

    Created screen (not implemented) to display the rooms the user is in

---
+++ app/src/main/java/com/example/picit/UserRoomsScreen.kt
@@ -0,0 +1,18 @@
+package com.example.picit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.picit.ui.theme.PicItTheme
+
+@Composable
+fun UserRoomsScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PicItTheme {
+        UserRoomsScreen()
+    }
+}
