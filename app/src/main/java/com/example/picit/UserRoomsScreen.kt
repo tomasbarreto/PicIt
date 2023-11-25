ec9303744c7cf9f9af5779aad3fba8ffe66f9689
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-25 15:18:06 +0000

    Screen title and SearchBard implemented

--- app/src/main/java/com/example/picit/UserRoomsScreen.kt
+++ app/src/main/java/com/example/picit/UserRoomsScreen.kt
@@ -1,12 +1,68 @@
 package com.example.picit
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.picit.ui.theme.PicItTheme
 
 @Composable
 fun UserRoomsScreen(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Spacer(modifier = Modifier.height(48.dp))
+        Row(
+            modifier = Modifier.
+                fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Your rooms", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        }
 
+        Spacer(modifier = Modifier.height(32.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            SearchBar()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar() {
+    var text by remember { mutableStateOf("") } // TODO: check if its this
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Search") },
+        leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+        modifier = Modifier.fillMaxWidth(0.8f)
+    )
 }
 
 @Preview(showBackground = true)
