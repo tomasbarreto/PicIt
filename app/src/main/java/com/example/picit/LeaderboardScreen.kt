94f6ccdba023c4c9170c096549b65620e2b9d3c8
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2023-11-26 16:41:22 +0000

    created RoomHeader Composable

--- app/src/main/java/com/example/picit/LeaderboardScreen.kt
+++ app/src/main/java/com/example/picit/LeaderboardScreen.kt
@@ -1,25 +1,30 @@
 package com.example.picit
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.picit.ui.theme.PicItTheme
 
 
 @Composable
-fun LeaderboardScreen(){
+fun LeaderboardScreen(modifier : Modifier = Modifier){
     Column (
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        RoomHeader()
         AppBottomMenu(inFriendScreen = false, inHomeScreen = false, inProfileScreen = false)
     }
 
 }
+
+
 
 
 @Preview(showBackground = true)
