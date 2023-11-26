444d2ae88b883b25c63237383d2d56a7e665f9f5
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2023-11-26 16:51:54 +0000

    added BackButton Composable

--- app/src/main/java/com/example/picit/LeaderboardScreen.kt
+++ app/src/main/java/com/example/picit/LeaderboardScreen.kt
@@ -1,14 +1,27 @@
 package com.example.picit
 
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.picit.ui.theme.PicItTheme
 
 
@@ -19,7 +32,20 @@ fun LeaderboardScreen(modifier : Modifier = Modifier){
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         RoomHeader()
+        BackButton()
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Leaderboard", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        }
+
+
+
         AppBottomMenu(inFriendScreen = false, inHomeScreen = false, inProfileScreen = false)
+
+
     }
 
 }
