a22e960b5fcac6d8c643fc15bdeee75b6d01f0c2
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-27 18:48:53 +0000

    Created compsable for the WatingPhotoDescriptionScreen

---
+++ app/src/main/java/com/example/picit/WaitingPhotoDescriptionScreen.kt
@@ -0,0 +1,18 @@
+package com.example.picit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WatingForPhotoDescriptionScreen(){
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        
+    }
+}
