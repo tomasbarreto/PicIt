89269516ad359d846f8700cd9633503a2dec4e44
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-27 20:08:36 +0000

    Screen to wait for photo description done, just needs the LeaderboardButton replaced

--- app/src/main/java/com/example/picit/WaitingPhotoDescriptionScreen.kt
+++ app/src/main/java/com/example/picit/WaitingPhotoDescriptionScreen.kt
@@ -6,16 +6,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,17 +40,46 @@ fun WaitingPhotoDescriptionScreen(){
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        var roomName = "Room name" // get this value dynamically
-        ScreenHeader(text = roomName, headerFontSize = 24.sp, withButton = true)
+        var roomName = "Room name" // get in database from the room fields
+        ScreenHeader(text = roomName, withButton = true)
+
+        var timeForDescriptionRelease = "16:00" // get in database from the room fields
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .fillMaxHeight(0.9f)
+        ){
+            Text(
+                text ="Comeback at $timeForDescriptionRelease for the photo description release...",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.Center),
+                fontSize = 40.sp,
+                lineHeight = 56.sp
+            )
+
+            Box(modifier = Modifier.align(Alignment.BottomEnd)){
+                LeaderboardButton()
+            }
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+        AppBottomMenu(inFriendScreen = false, inHomeScreen = true, inProfileScreen = false)
+    }
+}
+
+@Composable
+fun LeaderboardButton() {
+    Button(
+        onClick = { /*TODO*/ },
+    ) {
+        Icon(Icons.Outlined.Star, contentDescription = null,)
     }
 }
 
 
-
-
 @Preview(showBackground = true)
 @Composable
-fun PreviewWaitingPhotoDescritpionScreen() {
+fun PreviewWaitingPhotoDescriptionScreen() {
     PicItTheme {
         WaitingPhotoDescriptionScreen()
     }
