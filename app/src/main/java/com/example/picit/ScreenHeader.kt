c612f8136356844db22a6099ba0a8148bd55b67a
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-27 19:36:48 +0000

    Created ScreenHeader composable, to be used in all screens with a header

---
+++ app/src/main/java/com/example/picit/ScreenHeader.kt
@@ -0,0 +1,48 @@
+package com.example.picit
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ScreenHeader(withButton: Boolean=false,text:String, headerFontSize: TextUnit = 32.sp){
+    Spacer(modifier = Modifier.height(60.dp))
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        if(withButton){
+            Box(
+                modifier = Modifier.align(Alignment.CenterStart)
+            ){
+                BackButton(onButtonClick={/*TODO: go to UserRoomsScreen*/})
+            }
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.align(Alignment.Center).width(180.dp)
+        ) {
+            Text(
+                text = text,
+                fontSize = headerFontSize,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/picit/WaitingPhotoDescriptionScreen.kt
+++ app/src/main/java/com/example/picit/WaitingPhotoDescriptionScreen.kt
@@ -1,18 +1,48 @@
 package com.example.picit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.picit.ui.theme.PicItTheme
 
 @Composable
-fun WatingForPhotoDescriptionScreen(){
+fun WaitingPhotoDescriptionScreen(){
     Column (
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        
+        var roomName = "Room name" // get this value dynamically
+        ScreenHeader(text = roomName, headerFontSize = 24.sp, withButton = true)
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWaitingPhotoDescritpionScreen() {
+    PicItTheme {
+        WaitingPhotoDescriptionScreen()
     }
 }
