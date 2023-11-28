64643ee33a7568dcc77912fc9c83e5fa9efb3274
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-11-28 10:15:48 +0000

    Created composable to submit the photo description

---
+++ app/src/main/java/com/example/picit/SubmitPhotoDescriptionScreen.kt
@@ -0,0 +1,18 @@
+package com.example.picit
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.picit.ui.theme.PicItTheme
+
+@Composable
+fun SubmitPhotoDescriptionScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SubmitPhotoDescritpionScreenPreview() {
+    PicItTheme {
+        SubmitPhotoDescriptionScreen()
+    }
+}
