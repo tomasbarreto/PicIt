e00d61db53a24826a05d79899da1cfd6f9a2c351
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2023-11-28 17:13:06 +0000

    LoginScreen skeleton implemented

--- app/src/main/java/com/example/picit/MainActivity.kt
+++ app/src/main/java/com/example/picit/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             PicItTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    UserRoomsScreen()
+                    LoginScreen()
                 }
             }
         }
