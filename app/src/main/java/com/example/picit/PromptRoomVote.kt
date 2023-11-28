ef25031e59ad22b947edf4d13e18d451f2552679
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2023-11-28 15:43:10 +0000

    repic vote room changed to prompt vote room

--- app/src/main/java/com/example/picit/PromptRoomVote.kt
+++ app/src/main/java/com/example/picit/PromptRoomVote.kt
@@ -1,6 +1,7 @@
 package com.example.picit
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,14 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.picit.ui.theme.PicItTheme
 
 @Composable
-fun RepicRoomVote(){
+fun PromptRoomVote(){
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -42,16 +45,18 @@ fun RepicRoomVote(){
 
         Row (modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp)
-            .height(80.dp),
-            horizontalArrangement = Arrangement.Start,
+            .padding(20.dp)
+            .height(50.dp)
+            .background(Color.LightGray),
+            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically)
         {
-            Text(text="To Repic:", fontSize = 20.sp, modifier = Modifier.padding(start= 60.dp, end=20.dp))
-            Image(
-                painter = painterResource(id = R.drawable.imagetorepic),
-                contentDescription = "woman kissing the sunset"
-            )
+            Column {
+                Text(text = "Prompt:", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+            }
+            Column(modifier = Modifier.padding(start=5.dp)) {
+                Text(text = "Pose with the sunset", fontSize = 20.sp)
+            }
         }
 
         Row (modifier = Modifier
@@ -64,8 +69,12 @@ fun RepicRoomVote(){
             Column {
                 Row (modifier = Modifier.height(25.dp),
                     verticalAlignment = Alignment.Bottom){
-                    Icon(Icons.Filled.Person, contentDescription = null, modifier = Modifier.height(25.dp).fillMaxWidth(0.1F))
-                    Text(text = "User name", modifier = Modifier.height(22.dp).fillMaxWidth(0.55F))
+                    Icon(Icons.Filled.Person, contentDescription = null, modifier = Modifier
+                        .height(25.dp)
+                        .fillMaxWidth(0.1F))
+                    Text(text = "User name", modifier = Modifier
+                        .height(22.dp)
+                        .fillMaxWidth(0.55F))
                     Text(text="08:00", fontSize = 12.sp)
                 }
                 Image(
@@ -88,7 +97,7 @@ fun RepicRoomVote(){
         TimeLeftDisplay("Vote")
         Row(modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp, top=30.dp, bottom = 30.dp),
+            .padding(start = 20.dp, end = 20.dp, top = 30.dp, bottom = 30.dp),
             horizontalArrangement = Arrangement.End) {
             LeaderboardButton()
         }
@@ -102,6 +111,6 @@ fun RepicRoomVote(){
 @Composable
 fun RepicRoomVotePreview() {
     PicItTheme {
-        RepicRoomVote()
+        PromptRoomVote()
     }
 }
