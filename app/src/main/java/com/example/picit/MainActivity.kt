d2f99c199f5acf356dcdc5a574b12bfb2f095abe
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2023-12-05 19:41:08 +0000

    Extracted NavHost

--- app/src/main/java/com/example/picit/MainActivity.kt
+++ app/src/main/java/com/example/picit/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.picit.navigation.PicItNavHost
 import com.example.picit.navigation.Screens
 import com.example.picit.picdesccreateroom.ChooseGame
 import com.example.picit.ui.theme.PicItTheme
@@ -34,54 +35,7 @@ fun PicItApp() {
     // A surface container using the 'background' color from the theme
     val navController = rememberNavController()
     Scaffold {
-        NavHost(
-            navController = navController,
-            startDestination = Screens.Home.route,
-            modifier = Modifier.padding(it)
-        ) {
-            val bottomNavigationsList = listOf(
-                { navController.navigate(Screens.Friends.route){ launchSingleTop = true } },
-                { navController.navigate(Screens.Home.route){ launchSingleTop = true } },
-                { navController.navigate(Screens.Profile.route){ launchSingleTop = true } },
-                )
-            val onClickBackButton= {navController.popBackStack()}
-            composable(route= Screens.Home.route){
-                UserRoomsScreen(
-                    bottomNavigationsList= bottomNavigationsList,
-                    onClickJoinRoom = { navController.navigate(Screens.RoomsToJoin.route)},
-                    onClickCreateRoom = {navController.navigate(Screens.CreateRoomChooseGame.route)},
-                    onClickInvitesButton = {navController.navigate(Screens.InvitesNotifications.route)},
-                    onClickSettings = {navController.navigate(Screens.Settings.route)}
-                )
-            }
-            // To go to a friend make func (firendId) -> navigate(freindId)
-            composable(route= Screens.Friends.route){
-                FriendsListScreen(bottomNavigationsList= bottomNavigationsList)
-            }
-            composable(route = Screens.Profile.route){
-                UserProfileScreen(bottomNavigationsList = bottomNavigationsList)
-            }
-            composable(route= Screens.RoomsToJoin.route){
-                PreviewRoomsToJoinScreen(
-                    onClickBackButton = {onClickBackButton()}
-                )
-            }
-            composable(route= Screens.CreateRoomChooseGame.route){
-                ChooseGame(
-                    onClickBackButton = { onClickBackButton() }
-                )
-            }
-            composable(route = Screens.InvitesNotifications.route){
-                RoomInviteNotificationsScreen(
-                    onClickBackButton = {onClickBackButton()}
-                )
-            }
-            composable(route = Screens.Settings.route){
-                SettingsScreen(
-                    onClickBackButton = {onClickBackButton()}
-                )
-            }
-        }
+        PicItNavHost(navController = navController, modifier=Modifier.padding(it))
     }
 }
 
---
+++ app/src/main/java/com/example/picit/navigation/NavHost.kt
@@ -0,0 +1,66 @@
+package com.example.picit.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.picit.FriendsListScreen
+import com.example.picit.PreviewRoomsToJoinScreen
+import com.example.picit.RoomInviteNotificationsScreen
+import com.example.picit.SettingsScreen
+import com.example.picit.UserProfileScreen
+import com.example.picit.UserRoomsScreen
+import com.example.picit.picdesccreateroom.ChooseGame
+
+@Composable
+fun PicItNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Home.route,
+        modifier = modifier
+    ) {
+        val bottomNavigationsList = listOf(
+            { navController.navigate(Screens.Friends.route){ launchSingleTop = true } },
+            { navController.navigate(Screens.Home.route){ launchSingleTop = true } },
+            { navController.navigate(Screens.Profile.route){ launchSingleTop = true } },
+        )
+        val onClickBackButton= {navController.popBackStack()}
+        composable(route= Screens.Home.route){
+            UserRoomsScreen(
+                bottomNavigationsList= bottomNavigationsList,
+                onClickJoinRoom = { navController.navigate(Screens.RoomsToJoin.route)},
+                onClickCreateRoom = {navController.navigate(Screens.CreateRoomChooseGame.route)},
+                onClickInvitesButton = {navController.navigate(Screens.InvitesNotifications.route)},
+                onClickSettings = {navController.navigate(Screens.Settings.route)}
+            )
+        }
+        composable(route= Screens.Friends.route){
+            FriendsListScreen(bottomNavigationsList= bottomNavigationsList)
+        }
+        composable(route = Screens.Profile.route){
+            UserProfileScreen(bottomNavigationsList = bottomNavigationsList)
+        }
+        composable(route= Screens.RoomsToJoin.route){
+            PreviewRoomsToJoinScreen(
+                onClickBackButton = {onClickBackButton()}
+            )
+        }
+        composable(route= Screens.CreateRoomChooseGame.route){
+            ChooseGame(
+                onClickBackButton = { onClickBackButton() }
+            )
+        }
+        composable(route = Screens.InvitesNotifications.route){
+            RoomInviteNotificationsScreen(
+                onClickBackButton = {onClickBackButton()}
+            )
+        }
+        composable(route = Screens.Settings.route){
+            SettingsScreen(
+                onClickBackButton = {onClickBackButton()}
+            )
+        }
+    }
+}
