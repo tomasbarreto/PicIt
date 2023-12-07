c62da435152a366bb87c858f603ce1eeb01bc59d
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2023-12-07 08:14:45 +0000

    import removed from register screen

--- app/src/main/java/com/example/picit/register/RegisterScreen.kt
+++ app/src/main/java/com/example/picit/register/RegisterScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.picit.ScreenHeader
-import com.example.picit.picdesccreateroom.ChooseGame
 import com.example.picit.ui.theme.PicItTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
