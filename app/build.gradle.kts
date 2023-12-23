db7cd89ff3e543382de8010e5584465bc875f0b1
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2023-12-23 08:38:58 +0000

    Authentication dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    // Authentication dependencies
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
