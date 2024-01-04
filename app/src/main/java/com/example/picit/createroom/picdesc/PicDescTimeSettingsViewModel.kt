d8f37312e96ccc3547437b5c50272ab819519a82
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-01-04 20:51:43 +0000

    User now is the leaderboard when cerating a picDescRoom

--- app/src/main/java/com/example/picit/createroom/picdesc/PicDescTimeSettingsViewModel.kt
+++ app/src/main/java/com/example/picit/createroom/picdesc/PicDescTimeSettingsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.picit.createroom.picdesc
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.picit.entities.PicDescRoom
+import com.example.picit.entities.UserInLeaderboard
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 
@@ -23,15 +24,18 @@ class PicDescTimeSettingsViewModel: ViewModel() {
         currentUserRooms: List<String>,
         currentUserId: String
     ) {
-
-        val newPicDescRoom = PicDescRoom(name = roomName, currentCapacity = 1, maxCapacity = roomCapacity.toInt(), maxNumOfChallenges = roomNumChallenges.toInt(),
-            winnerAnnouncementTime = timeWinner, photoSubmissionOpeningTime = timePictureSubmissionStart,
-            photoSubmissionClosingTime = timePictureSubmissionEnd, descriptionSubmissionOpeningTime = timeDescSubmissionStart,
-            descriptionSubmissionClosingTime = timeDescSubmissionEnd, currentLeader = currentUserId, privacy = privacy, privacyCode = privacyCode)
-
         val database = Firebase.database
         val roomRef = database.getReference("picDescRooms").push()
+
+        val newPicDescRoom = PicDescRoom(id = roomRef.key,name = roomName, currentCapacity = 1,
+            maxCapacity = roomCapacity.toInt(), maxNumOfChallenges = roomNumChallenges.toInt(),
+            winnerAnnouncementTime = timeWinner, photoSubmissionOpeningTime = timePictureSubmissionStart,
+            photoSubmissionClosingTime = timePictureSubmissionEnd, descriptionSubmissionOpeningTime = timeDescSubmissionStart,
+            descriptionSubmissionClosingTime = timeDescSubmissionEnd, currentLeader = currentUserId,
+            privacy = privacy, privacyCode = privacyCode, leaderboard = listOf(UserInLeaderboard(currentUserId,0))
+        )
         roomRef.setValue(newPicDescRoom)
+
 
         updateUserRooms(currentUserRooms, currentUserId, roomRef.key.toString())
         onClickGoHomeScreen()
