ef7df1857c2e7dd20303dc709b4f2ddd93aa615e
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2024-01-20 10:24:38 +0000

    photo comparator class

---
+++ app/src/main/java/com/example/picit/repic/PhotoComparator.kt
@@ -0,0 +1,51 @@
+package com.example.picit.repic
+
+import android.graphics.Bitmap
+import androidx.core.graphics.get
+
+class PhotoComparator {
+
+    private lateinit var modelPhoto: Bitmap
+
+    fun setModelPhoto(modelPhoto: Bitmap) {
+        this.modelPhoto = modelPhoto
+    }
+    fun comparePhoto(photoToCompare: Bitmap): Float {
+        val width = getWidthForComparison(photoToCompare)
+        val height = getHeightForComparison(photoToCompare)
+
+        var counter = 0F
+
+        for (i in 0..height - 1) {
+            for (j in 0..width - 1) {
+                if (photoToCompare[i, j] == modelPhoto[i, j]) {
+                    counter++
+                }
+            }
+        }
+
+        return counter / (height * width)
+    }
+
+    private fun getWidthForComparison(photoToCompare: Bitmap): Int {
+        val width = if (modelPhoto.width < photoToCompare.width) {
+            modelPhoto.width
+        }
+        else {
+            photoToCompare.width
+        }
+
+        return width
+    }
+
+    private fun getHeightForComparison(photoToCompare: Bitmap): Int {
+        val height = if (modelPhoto.height < photoToCompare.height) {
+            modelPhoto.height
+        }
+        else {
+            photoToCompare.height
+        }
+
+        return height
+    }
+}
