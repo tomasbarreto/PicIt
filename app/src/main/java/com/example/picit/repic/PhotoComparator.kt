6219067f76f22d7a7018e335972e1ffe413ce07a
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2024-01-20 17:02:54 +0000

    comparation method changed

--- app/src/main/java/com/example/picit/repic/PhotoComparator.kt
+++ app/src/main/java/com/example/picit/repic/PhotoComparator.kt
@@ -16,12 +16,16 @@ class PhotoComparator {
 
         for (i in 0..(width - 1)) {
             for (j in 0..(height - 1)) {
-                if (modelMutableBitmap.getPixel(i, j) == photoToCompareMutableBitmap.getPixel(i, j)) {
+                if (inPixelRoom(modelMutableBitmap.getPixel(i, j), photoToCompareMutableBitmap.getPixel(i, j))) {
                     counter++
                 }
             }
         }
 
         return counter / (height * width)
+    }
+
+    private fun inPixelRoom(pixel1: Int, pixel2: Int): Boolean {
+        return pixel1 <= pixel2 + 20 && pixel1 >= pixel2 - 20
     }
 }
