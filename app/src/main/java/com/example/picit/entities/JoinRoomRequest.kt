d3463e4d931614f5df967d89d73a196be1d6444a
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2024-01-20 22:12:30 +0000

    added username, roomname and gametype to joinRoomRequest schema

--- app/src/main/java/com/example/picit/entities/JoinRoomRequest.kt
+++ app/src/main/java/com/example/picit/entities/JoinRoomRequest.kt
@@ -1,7 +1,8 @@
 package com.example.picit.entities
 
 data class JoinRoomRequest(
-    val userIdThatSentRequest: Int, // id of the user that sent the request
-    val roomId: Int, // id of the room to join
-
+    val senderName: String = "", // id of the user that sent the request
+    val roomId: String = "", // id of the room to join
+    val roomName: String = "",
+    val gameType: GameType = GameType.REPIC
 )
