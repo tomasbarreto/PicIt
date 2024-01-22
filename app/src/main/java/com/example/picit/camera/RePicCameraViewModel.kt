c4d9964d46220816513c048ce97361a701329d9f
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-01-22 15:25:52 +0000

    Fixed bug in raplcing photo for repic

--- app/src/main/java/com/example/picit/camera/RePicCameraViewModel.kt
+++ app/src/main/java/com/example/picit/camera/RePicCameraViewModel.kt
@@ -94,7 +94,7 @@ class RePicCameraViewModel: ViewModel() {
         )
         val index = room.currentNumOfChallengesDone
         var updatedSubmittedPhotosInChallenge = if(room.photosSubmitted.size==index) mutableListOf()
-                                    else room.photosSubmitted[index].filter{ it.userId == userId }.toMutableList()
+                                    else room.photosSubmitted[index].filter{ it.userId != userId }.toMutableList()
         updatedSubmittedPhotosInChallenge.add(photo)
 
         val photosSubmittedUpdated = room.photosSubmitted.toMutableList()
