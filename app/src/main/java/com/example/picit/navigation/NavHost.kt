861c090c65abcaa7840ea1a9cea26a1010932f3e
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-01-25 11:06:07 +0000

    Using LaunchedEffect to get updated room

--- app/src/main/java/com/example/picit/navigation/NavHost.kt
+++ app/src/main/java/com/example/picit/navigation/NavHost.kt
@@ -500,13 +500,19 @@ fun PicItNavHost(navController: NavHostController, modifier: Modifier = Modifier
             }
             // time to submit photo
             else if (checkInterval(currentTime,photoSubmissionOpeningTime,winnerAnnouncementTime)){
-                val photosSubmitted =
-                    if(currentPicDescRoom.allPhotosSubmitted.size == currentPicDescRoom.currentNumOfChallengesDone) emptyList()
-                    else currentPicDescRoom.allPhotosSubmitted[currentPicDescRoom.currentNumOfChallengesDone]
-                val photosUserDidntVote = photosSubmitted.filter{
-                    it.userId != currentUser.id && !it.usersThatVoted.contains(currentUser.id)
+
+                var photoDisplayed = PicDescPhoto()
+                var photosSubmitted = emptyList<PicDescPhoto>()
+                LaunchedEffect(currentPicDescRoom){
+                    photosSubmitted =
+                        if(currentPicDescRoom.allPhotosSubmitted.size == currentPicDescRoom.currentNumOfChallengesDone) emptyList()
+                        else currentPicDescRoom.allPhotosSubmitted[currentPicDescRoom.currentNumOfChallengesDone]
+                    val photosUserDidntVote = photosSubmitted.filter{
+                        it.userId != currentUser.id && !it.usersThatVoted.contains(currentUser.id)
+                    }
+                    photoDisplayed= if (photosUserDidntVote.isNotEmpty()) {photosUserDidntVote[0]}
+                                    else PicDescPhoto()
                 }
-                val photoDisplayed = if (photosUserDidntVote.isNotEmpty()) {photosUserDidntVote[0]} else PicDescPhoto()
 
                 val picDescCameraViewModel: PicDescCameraViewModel = viewModel()
 
